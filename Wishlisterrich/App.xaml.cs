0fbfb0add30a855e927d6ef4b5495a34c110265d
Fyn <thereduzer897@gmail.com>
2024-07-09 23:33:33 +0200

    Init

---
+++ Wishlisterrich/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Wishlisterrich
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Wishlisterrich/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wishlisterrich
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
